52c73acb489e248b71f1be0108ae8465386d86f6
avi <hi@avi.im>
2022-02-10 20:24:23 +0530

    add kv store

--- disk_store.py
+++ disk_store.py
@@ -1,10 +1,50 @@
+import os.path
+import time
+from typing import Optional
+
+from format import KeyEntry, encode_kv, decode_kv
+
+# https://docs.python.org/3.7/tutorial/inputoutput.html#methods-of-file-objects
+DEFAULT_WHENCE = 0
+
+
 class DiskStorage:
 
-    def __init__(self):
-        raise NotImplementedError
+    def __init__(self, file_name: str = "data.db"):
+        self.file_name = file_name
+        self.write_position = 0
+        self.key_dir: dict[str, KeyEntry] = {}
+        # if the file exists already, then we will load the key_dir
+        if os.path.exists(file_name):
+            self._init_key_dir()
+        else:
+            self.file = open(file_name, 'a+b')
 
     def set(self, key: str, value: str):
-        raise NotImplementedError
+        timestamp = int(time.time())
+        sz, data = encode_kv(timestamp=timestamp, key=key, value=value)
+        self._write(data)
+        kv = KeyEntry(timestamp=timestamp, position=self.write_position, total_size=sz)
+        self.key_dir[key] = kv
+        # update last write
+        self.write_position += sz
 
     def get(self, key) -> str:
-        raise NotImplementedError
+        kv: Optional[KeyEntry] = self.key_dir.get(key)
+        if not kv:
+            return ""
+        self.file.seek(kv.position, DEFAULT_WHENCE)
+        data = self.file.read(kv.total_size)
+        _, _, value = decode_kv(data)
+        return value
+
+    def _write(self, data: bytes):
+        self.file.write(data)
+
+    # we will initialise the
+    def _init_key_dir(self):
+        pass
+
+    def close(self):
+        os.fsync(self.file)
+        self.file.close()
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from memory_store import MemoryStorage
 from disk_store import DiskStorage
 
@@ -10,14 +12,38 @@ def memory_db():
 
 
 def store_db():
-    store = DiskStorage()
+    store = DiskStorage("data.db")
     # on the first run, this will print empty string, but on the next run
     # it should print the value from the disk
     print(store.get("name"))
     store.set("name", "haha")
     print(store.get("name"))
+    store.close()
+
+
+def store_books():
+    # remove existing db
+    os.remove("data.db")
+    store = DiskStorage("data.db")
+    books = {
+        "crime and punishment": "dostoevsky",
+        "anna karenina": "tolstoy",
+        "war and peace": "tolstoy",
+        "hamlet": "shakespeare",
+        "othello": "shakespeare",
+        "brave new world": "huxley",
+        "dune": "frank herbert"
+    }
+    for k, v in books.items():
+        store.set(k, v)
+        print(F"set k={k}, v={v}")
+        print(F"get k={k}, v={store.get(k)}")
+
+    for k in books.keys():
+        print(F"get k={k}, v={store.get(k)}")
 
 
 if __name__ == '__main__':
     memory_db()
     store_db()
+    store_books()
