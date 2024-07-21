0f39594eb46c4dd6f27c73becf565d9128bbc5d8
Wattyyy <yuji.msec.ttasb@gmail.com>
2024-07-21 15:48:16 +0900

    Fix typos in example codes

--- src/caskdb/disk_store.py
+++ src/caskdb/disk_store.py
@@ -13,7 +13,7 @@ we cannot use the database.
 
 Typical usage example:
 
-    disk: DiskStorage = DiskStore(file_name="books.db")
+    disk: DiskStorage = DiskStorage(file_name="books.db")
     disk.set(key="othello", value="shakespeare")
     author: str = disk.get("othello")
     # it also supports dictionary style API too:
